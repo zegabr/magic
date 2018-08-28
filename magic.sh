b107981f488adcc8d86750670890a2f739583180
Jose Gabriel Silva Pereira <jgsp2@ug3c30.windows.cin.ufpe.br>
2018-08-28 09:37:20 -0300

    ready

---
+++ magic.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/zegabr/cps.git
+git clone https://github.com/zegabr/remaper.git
+
+rm cps/README.md
+rm cps/LICENSE
+mv cps ~/Desktop
+cd Desktop/cps
+bash cps.sh
+
+cd
+mv remaper/remaper.sh ~/Desktop/cps
