a72acef48875bc6ec1696ee09a146c6310657d7d
Jose Gabriel Silva Pereira <jgsp2@ug3c41.windows.cin.ufpe.br>
2018-09-20 07:13:02 -0300

    fast template added

---
+++ n.sh
@@ -0,0 +1,23 @@
+echo "#include <bits/stdc++.h>
+using namespace std;
+char bb = '\n',sp=' ';
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    return 0;
+}
+" > a.cpp
+
+##gedit settings
+gsettings set org.gnome.gedit.preferences.print print-syntax-highlighting true
+gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+gsettings set org.gnome.gedit.preferences.editor bracket-matching true
+gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor insert-spaces false
+gsettings set org.gnome.gedit.preferences.editor auto-indent true
+gsettings set org.gnome.gedit.preferences.editor syntax-highlighting true
+gsettings set org.gnome.gedit.preferences.editor display-overview-map false
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4 
+gsettings set org.gnome.gedit.preferences.editor wrap-mode 'none'
+wmctrl -r gedit -e 0,0,150,430,570 ##change gedit window position and dimensions
+gedit a.cpp i &
