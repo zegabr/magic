de302ef201afd4b9396ef6fcf179b44d42e1e554
zegabr <jgsp2@cin.ufpe.br>
2018-12-13 13:12:12 -0300

    alias novos

--- a.sh
+++ a.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-alias boracodar='cd && bash m/n.sh'
 alias python=python3
+alias draft='cd && bash m/n.sh'
+alias contest='cd && bash m/m.sh'
 alias acpp='g++ a.cpp -std=c++17 -Wfatal-errors -O3'
+alias Desktop='cd && cd Área\ de\ Trabalho/'
+echo "you now have new aliases"
+echo
+alias
 
