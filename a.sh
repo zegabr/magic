6d3c0a1587b82111817eadcf94c4f14a249cf894
zegabr <jgsp2@cin.ufpe.br>
2019-01-05 18:44:51 -0300

    now overrides every time

--- a.sh
+++ a.sh
@@ -1,13 +1,26 @@
 #cria aliases
+unalias py
 alias py=python3
 
+unalias draft
 alias draft='cd && bash m/n.sh'
+
+unalias contest
 alias contest='cd && bash m/m.sh'
+
+unalias acpp
 alias acpp='g++ a.cpp -std=c++17 -Wfatal-errors -O3'
 
+
+unalias Desktop
 alias Desktop='cd && cd Desktop/'
 
+
+unalias remaper
 alias remaper='bash ~/remaper.sh'
+
+
+unalias undoremaper
 alias undoremaper='bash ~/undo.sh'
 
 echo "you now have new aliases"
