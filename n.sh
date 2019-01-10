27aac1cb34ecaae41238f18a527eb85b8f4af4fa
zegabr <jgsp2@cin.ufpe.br>
2019-01-10 11:49:16 -0300

    vim is not friendly with cinufpe

--- n.sh
+++ n.sh
@@ -28,15 +28,15 @@ cp a.cpp tmp.cpp
 echo "paste input here" > i
 
 ##gedit settings
-echo "bash geditsetup.sh for configuring gedit"
-echo
+bash geditsetup.sh 
+#echo
 ##vim settings
-echo "cat m/vimsetup > ~/.vimrc for vimsetup"
-echo
-vim --version | grep +clipboard
-vim --version | grep +autocmd
-echo "if it has +clipboard, you can use ^A, ^C and ^V"
-echo
+#echo "cat m/vimsetup > ~/.vimrc for vimsetup"
+#echo
+#vim --version | grep +clipboard
+#vim --version | grep +autocmd
+#echo "if it has +clipboard, you can use ^A, ^C and ^V"
+#echo
 
-echo "a.cpp and i (input file) added to your home folder"
+echo "tmp.cpp, a.cpp and i (input file) added to your home folder"
 
