b0a0c2112131e9d5a2cc13ea196dacaaee058a51
zegabr <jgsp2@cin.ufpe.br>
2019-01-12 14:01:21 -0300

    remaper and undoremaper alias work everywere

--- a.sh
+++ a.sh
@@ -2,15 +2,8 @@
 unalias py
 alias py=python3
 
-unalias draft
-alias draft='cd && bash m/n.sh'
-
-unalias contest
-alias contest='cd && bash m/m.sh'
-
 unalias acpp
 alias acpp='g++ a.cpp -std=c++17 -Wfatal-errors -O3'
-
 
 unalias Desktop
 alias Desktop='cd && cd Desktop/'
@@ -18,7 +11,6 @@ alias Desktop='cd && cd Desktop/'
 
 unalias remaper
 alias remaper='bash ~/remaper.sh'
-
 
 unalias undoremaper
 alias undoremaper='bash ~/undo.sh'
