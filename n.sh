ebf604164ce1d0cf99d1776aa083a2b41a2d9e9d
zegabr <jgsp2@cin.ufpe.br>
2019-01-20 17:35:53 -0300

    tmp.cpp updated

--- n.sh
+++ n.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 ## draft creation
-echo "#include <bits/stdc++.h>
-using namespace std;
-#define pl '\n'
-#define sp ' '
-#define Y \"YES\"
-#define N \"NO\"
-typedef long long ll;
-typedef pair<ll,ll> pll;
-
-
-
-
-
-
-
-int main(){
-	ios::sync_with_stdio(0); cin.tie(0);
-	//cout<<fixed<<setprecision(10);
-			
-
-
-
-
-
-}" > a.cpp
-cp a.cpp tmp.cpp
+cp  ./m/tmp.cpp ./a.cpp
+cp ./a.cpp ./tmp.cpp
 echo "paste input here" > i
 ##gedit settings
 bash ./m/geditsetup.sh 
---
+++ tmp.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <bitset>
+#include <cmath>
+#include <climits>
+//#include <string>//testar swap sem este include
+//#include <utility>//testar pair sem este include
+
+#define pb(x) push_back(x) 
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define fr(i, l, r) for(int i=l;i<r;i++)//l..r-1
+#define frb(i, r, l) for(int i=r;i>l;i--)//r..l+1
+#define fi first
+#define se second
+
+using namespace std;
+
+#define pl '\n'
+#define sp ' '
+#define yes "YES"
+#define no "NO"
+
+typedef long long ll;
+typedef pair<ll,ll> pll;
+typedef pair<int,int> ii;
+
+const ll mod=1e9+7;
+const ll N=1e5+5;
+
+
+
+
+
+int main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+	//cout<<fixed<<setprecision(10);
+			
+
+
+
+
+
+}
