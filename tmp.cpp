ab675bea0a0b55f2f774e3e3470a54090a39f8eb
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-01-21 17:22:33 -0300

    macros and includes recuced - code too big

--- tmp.cpp
+++ tmp.cpp
@@ -2,39 +2,21 @@
 #include <iomanip>
 #include <vector>
 #include <algorithm>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <bitset>
 #include <cmath>
-
-#define debug 1
-#if debug
-#endif
 
 #define pb push_back
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
-#define rep(i, a, b) for(int i=a;i<=b;i++)//a..b
-#define repb(i, b, a) for(int i=b;i>=a;i--)//b..a
-#define fi first
-#define se second
+#define rep(i, a, b) for(int i=a;i<=b;i++)
 
 #define pl '\n'
 #define sp ' '
 #define yes "YES\n"
 #define no "NO\n"
 
-#define inf INT32_MAX
-
 using namespace std;
 
 typedef long long ll;
-typedef pair<ll,ll> pll;
-typedef pair<int,int> ii;
 
 const ll mod=1e9+7;
 const ll N=1e5+5;
