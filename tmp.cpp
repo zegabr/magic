2f84aaefcb9a2c2cc3ac64e0dea7820858eae904
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-01-22 16:48:36 -0300

    template beautified

--- tmp.cpp
+++ tmp.cpp
@@ -4,15 +4,14 @@
 #include <algorithm>
 #include <cmath>
 
+#define sp ' '
+#define pl '\n'
+#define no "NO\n"
+#define yes "YES\n"
 #define pb push_back
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for(int i=a;i<=b;i++)
-
-#define pl '\n'
-#define sp ' '
-#define yes "YES\n"
-#define no "NO\n"
 
 using namespace std;
 
@@ -25,7 +24,6 @@ const ll N=1e5+5;
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	//cout<<fixed<<setprecision(10);
-	
 		
 
 }
