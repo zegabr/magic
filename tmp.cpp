7d16c38cd76baaabe7f352a61e6c6cd9c889dbe7
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-01-24 20:00:33 -0300

    vim friendly template

--- tmp.cpp
+++ tmp.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-
+#include<algorithm>
+#include<iostream>
+#include<sstream>
+#include<iomanip>
+#include<bitset>
+#include<vector>
+#include<cmath>
+#include<stack>
+#include<queue>
+#include<map>
+#include<set>
 #define sp ' '
 #define pl '\n'
 using namespace std;
@@ -12,9 +17,10 @@ typedef long long ll;
 
 
 
+
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	//cout<<fixed<<setprecision(10);
-		
+	
 
 }
