bc0accd10548eb05ae5453d1b82969b5c3a02742
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-01-24 22:26:46 -0300

    vimrc update

--- n.sh
+++ n.sh
@@ -3,16 +3,12 @@
 cp  ./m/tmp.cpp ./x.cpp
 cp ./x.cpp ./tmp.cpp
 
-
 ##gedit settings
 bash ./m/geditsetup.sh 
 
-
-
-
 ##vim settings
-echo "cat m/vimsetup > ~/.vimrc for vimsetup"
-echo
+cat m/vimsetup > ./.vimrc
+echo ".vimrc created at current directory"
 vim --version | grep +clipboard
 vim --version | grep +autocmd
 echo "if it has +clipboard, you can use ^A, ^C and ^V"
