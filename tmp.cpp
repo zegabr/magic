6959840e30883bdcadbda7e2f1e83782551ce669
Jose Gabriel Silva Pereira <jgsp2@ug3c50.windows.cin.ufpe.br>
2019-01-25 16:43:42 -0300

    321

--- tmp.cpp
+++ tmp.cpp
@@ -1,11 +1,8 @@
 #include<bits/stdc++.h>
 #define sp ' '
 #define pl '\n'
-#define rep(i,a,b) for(int i=a; i<=(int)b; i++)
 using namespace std;
 typedef long long ll;
-typedef long double ld;
-
 
 
 
