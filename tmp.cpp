4999d0d538697486607d8bdd902fa2f8e8f9a94e
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-01-26 16:16:03 -0300

    tmp update

--- tmp.cpp
+++ tmp.cpp
@@ -1,14 +1,15 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std; typedef long long ll;
 #define sp ' '
 #define pl '\n'
-using namespace std;
-typedef long long ll;
+const int N = 5 + 1e5 * 3;
 
 
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
-	//cout<<fixed<<setprecision(10);
-	
+
 
 }
