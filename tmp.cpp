4ce4f0e7a9b186a13f621c6f34c4b64790c5281b
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-02-09 09:49:53 -0300

    tmplate update

--- tmp.cpp
+++ tmp.cpp
@@ -1,13 +1,11 @@
 #include<bits/stdc++.h>
 #define sp ' '
 #define pl '\n'
-#define fi first
-#define se second
-#define mod(X) (X)%MOD
 #define rep(i,a,b) for(int i = a; (a>b? i>=b : i<=b) ; (a>b? i-- : i++) )
-using namespace std; typedef long long ll;
-const int N = 5 + 1e5 * 3;
+using namespace std;
+typedef long long ll;
 const int MOD = 1e9+7;
+const int N = 5 + 1e5 * 3;
 
 
 
