35a391424f53bd1a9d10773c30d559fd734c9fa8
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-03-04 14:01:21 -0300

    git aliases thanks internet

--- a.sh
+++ a.sh
@@ -3,6 +3,15 @@
 unalias py
 alias py=python3
 
+unalias gs
+alias gs='git status '
+unalias ga
+alias ga='git add '  
+unalias gaa
+alias gaa='git add --all'
+unalias gcm
+alias gcm='git commit -m '
+
 #unalias remaper
 #alias remaper='bash ~/remaper.sh'
 #unalias undoremaper
