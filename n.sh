de00d07726033d9783f8f45dd5d67ce6b8572735
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-03-05 14:40:00 -0300

    notification added for default x

--- n.sh
+++ n.sh
@@ -3,7 +3,8 @@
 cp  ./m/tmp.cpp ./x.cpp
 cp ./x.cpp ./tmp.cpp
 cp ./m/Makefile ./Makefile
-
+echo "========x.cpp reseted==========
+"
 source ./m/g.sh ##gedit settings
 source ./m/v.sh ##vim settings
 
