af342377d9c665cf74dc530937761fb83a723c2c
Jose Gabriel Silva Pereira <jgsp2@ug2c06.windows.cin.ufpe.br>
2019-04-03 15:07:05 -0300

    ESS setup working

--- e.sh
+++ e.sh
@@ -1,14 +1,27 @@
 #e de ESS
+cd ~/Desktop
+mkdir ESS
+cd ESS
 
 npm install @angular/cli
 npm install typescript
 
-#PATH=$PATH:/home/CIN/jgsp2/node_modules/@angular/cli/bin:/home/CIN/jgsp2/node_modules/typescript/bin
+
+cd node_modules/@angular/cli/bin
+pwd > ~/path1
+
+cd ~/Desktop/ESS/node_modules/typescript/bin
+pwd > ~/path2
 
 
-#teacher asked to do as below only one time, but maybe is enough to do the above all times, because works
-echo "
-PATH=$PATH:/home/CIN/jgsp2/node_modules/@angular/cli/bin:/home/CIN/jgsp2/node_modules/typescript/bin" >> ~/.bashrc
+printf "
+PATH=\$PATH:" >> ~/.bashrc
+cat ~/path1 >> ~/.bashrc
+
+printf "
+PATH=\$PATH:" >> ~/.bashrc
+cat ~/path2 >> ~/.bashrc
+
 source ~/.bashrc
 
 
