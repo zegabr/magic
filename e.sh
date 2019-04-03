a13abbbc7b72b96263f1b1d5e573b57ba217e98d
Jose Gabriel Silva Pereira <jgsp2@ug2c13.windows.cin.ufpe.br>
2019-04-03 14:05:19 -0300

    path variable directly at  bashrc

--- e.sh
+++ e.sh
@@ -3,11 +3,12 @@
 npm install @angular/cli
 npm install typescript
 
-PATH=$PATH:/home/CIN/jgsp2/node_modules/@angular/cli/bin:/home/CIN/jgsp2/node_modules/typescript/bin
+#PATH=$PATH:/home/CIN/jgsp2/node_modules/@angular/cli/bin:/home/CIN/jgsp2/node_modules/typescript/bin
 
 
 #teacher asked to do as below only one time, but maybe is enough to do the above all times, because works
-#echo "PATH=$PATH:/home/CIN/jgsp2/node_modules/@angular/cli/bin:/home/CIN/jgsp2/node_modules/typescript/bin" >> ~/.bashrc
-#source ~/.bashrc
+echo "
+PATH=$PATH:/home/CIN/jgsp2/node_modules/@angular/cli/bin:/home/CIN/jgsp2/node_modules/typescript/bin" >> ~/.bashrc
+source ~/.bashrc
 
 
