a2dcd015ac4ffb839be3364260abe261a58913f1
Jose Gabriel Silva Pereira <jgsp2@ug2c07.windows.cin.ufpe.br>
2019-04-05 20:42:39 -0300

    bug

--- a.sh
+++ a.sh
@@ -11,12 +11,12 @@ unalias gaa
 alias gaa='git add --all'
 unalias gcm
 alias gcm='git commit -m '
-:'
-unalias remaper
-alias remaper='bash ~/remaper.sh'
-unalias undoremaper
-alias undoremaper='bash ~/undo.sh'
-'
+
+#unalias remaper
+#alias remaper='bash ~/remaper.sh'
+#unalias undoremaper
+#alias undoremaper='bash ~/undo.sh'
+
 echo "==============you have new aliases============="
 
 
