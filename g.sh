9c799aa1f790569aa10a8fd4cc69d63228004e25
Rodrigo Farias Rodrigues Lemos <rfrl@ug3c37.windows.cin.ufpe.br>
2019-04-27 19:31:56 -0300

    tab 2

--- g.sh
+++ g.sh
@@ -9,6 +9,6 @@ gsettings set org.gnome.gedit.preferences.editor insert-spaces false
 gsettings set org.gnome.gedit.preferences.editor auto-indent true
 gsettings set org.gnome.gedit.preferences.editor syntax-highlighting true
 gsettings set org.gnome.gedit.preferences.editor display-overview-map false
-gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.editor tabs-size 2
 gsettings set org.gnome.gedit.preferences.editor wrap-mode 'none'
 echo "===========my gedit setup overrided yours==========="
