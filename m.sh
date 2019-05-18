148789d4c32698dd428bb627b6f56d2e336c0b50
zegabr <jgsp2@cin.ufpe.br>
2019-05-17 23:49:22 -0300

    easy reset from training dir using r.sh which does same thing as n.sh

--- m.sh
+++ m.sh
@@ -7,19 +7,8 @@ rm -rf cp/
 git clone https://github.com/zegabr/cp.git
 
 
-:'
-git clone https://github.com/zegabr/remaper.git
-mv ./remaper/remaper.sh ~/
-mv ./remaper/undo.sh ~/
-rm -f -r ./remaper
-echo "
------to remap keys or undo remapping, use remaper and undoremaper aliases-----
-"
-'
-
-clear
-
 source ../m/n.sh #rascunho + template + aliases
+cp ../m/n.sh ./r.sh
 echo 
 echo "cp folder has:"
 ls cp/
