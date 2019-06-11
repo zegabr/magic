80b4ea5ca410a3050b0bcfc35de3bf741dce72d8
zegabr <jgsp2@cin.ufpe.br>
2019-06-11 20:15:50 -0300

    online competitive template in progress

---
+++ Tmp.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define sp ' '
+#define pl '\n'
+using namespace std;
+typedef long long ll;
+
+#define bpopc(x) __builtin_popcount(x)
+#define sci(x) scanf("%d",&x)
+#define scll(x) scanf("%lld",&x)
+#define all(x) (x).begin(),(x).end()
+#define pb(x) push_back(x)
+#define fr(i,a,b) for(int i = a; (a>b? i>=b : i<=b) ; (a>b? i-- : i++) )
+#define x first//testar isso com declaracoes de x y fiar e sedond
+#define y second
+
+
+//#define db(x) 
+//#define int ll
+
+const int mx = 2e5+5;
+const int mod = 1e9+7;
+const int inf =0x3f3f3f3f;
+
+main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+
+}
