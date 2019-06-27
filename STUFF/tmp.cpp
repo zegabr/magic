3f1accaa0983eef74f1c2cac0176c1f491fd277b
zegabr <jgsp2@cin.ufpe.br>
2019-06-27 16:15:36 -0300

    include string and limits unecessary

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -1,28 +1,20 @@
-#include <set>
-#include <map>
 #include <cmath>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <tuple>
-#include <bitset>
-#include <limits>
-#include <string>
 #include <vector>
-#include <iomanip>
 #include <iostream>
 #include <algorithm>
-#include <unordered_map>
-#include <unordered_set>
+
+#define pl endl
+#define endl '\n'
 
 using namespace std;
+
 typedef long long ll;
 #define int ll
-#define endl '\n'
 
 
 
 main(){
 	ios::sync_with_stdio(0); cin.tie(0);
+	
 
 }
