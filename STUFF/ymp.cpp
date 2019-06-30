aa39ba9bb47c9c60287fdd7bd0ad3cdcd2d1edce
zegabr <jgsp2@cin.ufpe.br>
2019-06-30 18:27:31 -0300

    testcase

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -19,8 +19,6 @@
 
 #define pb push_back
 #define mp make_pair
-#define sci(x) scanf("%d",&x)
-#define scll(x) scanf("%lld",&x)
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 #define biton(x,i) ((x>>i)&1)
@@ -29,16 +27,17 @@
 #define popcount(x) __builtin_popcount(x)
 #define popcountll(x) __builtin_popcountll(x)
 #define precision(n) cout<<fixed<<setprecision(n)
+#define testcase(n) cout<<"Case #"<<n<<": "
 #define db(x) cerr << #x<< " = " << x << endl
 #define getpos(c,x) (lower_bound(all(c),x)-c.begin())
 #define fr(i,a,b) for(int i=a;(a>b?i>=b:i<=b);(a>b?i--:i++))
-#define fastios 	ios::sync_with_stdio(0); cin.tie(0)
-#define fi first
-#define se second
+#define fastios ios::sync_with_stdio(0); cin.tie(0)
 #define mset(x,y) memset(&x, (y), sizeof(x))
 #define PI 3.141592653589793238462643383279502884
 #define INF 0x3f3f3f3f
 #define INFLL 0x3f3f3f3f3f3f3f3f
+#define x first
+#define y second
 #define pl endl
 #define endl '\n'
 using namespace std;
