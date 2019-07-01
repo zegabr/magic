a8020501899c872c5d050404413fb247948a1086
Jose Gabriel Silva Pereira <jgsp2@ug2c06.windows.cin.ufpe.br>
2019-07-01 09:40:35 -0300

    g is smaller than g++ huehuehu

--- a.sh
+++ a.sh
@@ -4,18 +4,20 @@ unalias py
 alias py=python3
 unalias m
 alias m='make && make t'
-unalias g++
-alias g++='g++ -std=c++11 -O2 -Wfatal-errors'
+unalias g
+alias g='g++ -std=c++11 -O2'
+unalias gw
+alias gw='g++ -std=c++11 -O2 -Wfatal-errors'
 
 
 unalias gs
-alias gs='git status '
+alias gs='git status'
 unalias ga
-alias ga='git add '  
+alias ga='git add'  
 unalias gaa
 alias gaa='git add --all'
 unalias gcm
-alias gcm='git commit -m '
+alias gcm='git commit -m'
 
 #unalias remaper
 #alias remaper='bash ~/remaper.sh'
