a4543338edbbe08d88584a67b46848e2330f6630
zegabr <jgsp2@cin.ufpe.br>
2019-07-05 20:57:38 -0300

    M_PI exists
    heap is inteligent

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -17,49 +17,35 @@
 // #include <complex>
 // #include <sstream>
 
-#define pb push_back
-#define mp make_pair
-#define sci(x) scanf("%d",&x)
-#define scll(x) scanf("%lld",&x)
-#define all(x) (x).begin(),(x).end()
-#define rall(x) (x).rbegin(),(x).rend()
-#define biton(x,i) ((x>>i)&1)
-#define setbit(x,i) x|=(1<<i)
-#define togbit(x,i) x^=(1<<i)
-#define popcount(x) __builtin_popcount(x)
-#define popcountll(x) __builtin_popcountll(x)
-#define precision(n) cout<<fixed<<setprecision(n)
-#define db(x) cerr << #x<< " = " << x << endl
-#define getpos(c,x) (lower_bound(all(c),x)-c.begin())
-#define fr(i,a,b) for(int i=a;(a>b?i>=b:i<=b);(a>b?i--:i++))
-#define fastios 	ios::sync_with_stdio(0); cin.tie(0)
-#define fi first
-#define se second
-#define mset(x,y) memset(&x, (y), sizeof(x))
-#define PI 3.141592653589793238462643383279502884
-#define INF 0x3f3f3f3f
-#define INFLL 0x3f3f3f3f3f3f3f3f
+
 #define pl endl
 #define endl '\n'
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define all(k) (k).begin(),(k).end()
+#define mset(a,b) memset(&a, (b), sizeof(a))
+#define db(k) cerr << #k<< " = " << k << endl
+#define iofast 	ios::sync_with_stdio(0); cin.tie(0)
+#define getpos(c,k) (lower_bound(all(c),k)-c.begin())
+#define fr(i,a,b) for(int i=a;(a>b?i>=b:i<=b);(a>b?i--:i++))
 using namespace std;
-typedef unsigned long long ull;
-typedef unsigned int uint;
-typedef long double ld;
 typedef long long ll;
-
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
 // #define int ll
 
+#define heap priority_queue
 typedef vector<int> vi;
 typedef pair<int,int> pii;
-typedef priority_queue<int> Mheap;
-typedef priority_queue<int, vi, greater<int>> mheap;
 const int N = 2e5+5;
 const int mod = 1e9+7;
 
 
 
 main(){
-	fastios;
-	
+	iofast;
 
 }
