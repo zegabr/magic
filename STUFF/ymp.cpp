7d275f1269e5eaa93dadb25aeb9ef4f92d8be12a
zegabr <jgsp2@cin.ufpe.br>
2019-07-12 23:32:47 -0300

    simplification

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -24,32 +24,29 @@
 #define pb push_back
 #define mp make_pair
 #define all(k) (k).begin(),(k).end()
-#define mset(a,b) memset(&a, (b), sizeof(a))
+#define rall(k) (k).rbegin(),(k).rend()
 #define db(k) cerr << #k<< " = " << k << endl
-#define iofast 	ios::sync_with_stdio(0); cin.tie(0)
 #define getpos(c,k) (lower_bound(all(c),k)-c.begin())
 #define fr(i,a,b) for(int i=a;(a>b?i>=b:i<=b);(a>b?i--:i++))
 
 #define heap priority_queue
 #define hashset unordered_set
 #define hashmap unordered_map
-using namespace std;
-using ll = long long;
-using ld = long double;
-using uint = unsigned int;
-using ull = unsigned long long;
+#define ld long double
+#define ll long long
 // #define int ll
 
-using vi = vector<int>;
-using pii = pair<int,int>;
+using namespace std;
+
 const double PI = acos(-1);
 const int MOD = 1e9+7;
 const int N = 2e5+5;
 
 
-main(){
+int main(){
+ 	ios::sync_with_stdio(0); cin.tie(0);
 	//freopen("input","r",stdin);
-	iofast;
+
 
 
 
