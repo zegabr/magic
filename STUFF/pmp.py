64eb87e97221980730d02fa12cbbcb78f05c7704
zegabr <jgsp2@cin.ufpe.br>
2019-07-20 20:51:35 -0300

    faster?

--- STUFF/pmp.py
+++ STUFF/pmp.py
@@ -1,5 +1,3 @@
-from math import ceil,floor,fabs,factorial,fsum,gcd,log,pow as fpow,sqrt,acos,asin,atan,cos,sin,tan,degrees,radians,pi as PI,hypot
-from sys import stdin
 '''
 allinput = stdin.read() #read all input as string (till EOF)
 line = stdin.readline() #read one line as string (do not cause errors if eof)
@@ -11,21 +9,23 @@ print(a,b,c)
 '''builtin usefull functions: pow,sum,len,abs,max,min,sorted,reversed,bin,chr,ord,hex,oct,list,set,dict'''
 '''set() interface: add, discard, clear, in, not in, |, &, ^,update(iteralbe)'''
 '''dict() interface: d[key]=value, key in d, key not in d, clear, update(iteralbe)'''
-
+from math import ceil,floor,fabs,factorial,fsum,gcd,log,pow as fpow,sqrt,acos,asin,atan,cos,sin,tan,degrees,radians,pi as PI,hypot
+from sys import stdin
+line = stdin.readline
 def read1int():
-	a = int(stdin.readline().strip())
+	a = int(line().strip())
 	return a
 
 def read2int():
-	a,b = [int(x) for x in stdin.readline().strip().split()]
+	a,b = [int(x) for x in line().strip().split()]
 	return a,b
 
 def read3int():
-	a,b,c = [int(x) for x in stdin.readline().strip().split()]
+	a,b,c = [int(x) for x in line().strip().split()]
 	return a,b,c
 
 def read4int():
-	a,b,c,d = [int(x) for x in stdin.readline().strip().split()]
+	a,b,c,d = [int(x) for x in line().strip().split()]
 	return a,b,c,d
 
 #==========main===========
