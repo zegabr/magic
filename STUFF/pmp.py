94a0e32c1c4155919ca420292cec7b81d274f2a2
zegabr <jgsp2@cin.ufpe.br>
2019-07-20 18:10:51 -0300

    python "template"

---
+++ STUFF/pmp.py
@@ -0,0 +1,29 @@
+from sys import stdin
+'''
+allinput = stdin.read() #read all input as string (till EOF)
+line = input() #read one line as string
+char = stdin.read(1) #read one char
+file = open("arq.in",'r') #now file has read() and readline()
+a,b,c = sorted([int(x) for x in input().strip().split()]) #multiple init
+print(a,b,c)
+'''
+from math import ceil,floor,fabs,factorial,fsum,gcd,log,pow as fpow,sqrt,acos,asin,atan,cos,sin,tan,degrees,radians,pi as PI,hypot
+'''builtin usefull functions: pow,sum,len,abs,max,min,sorted,reversed,bin,chr,ord,hex,oct,list,set,dict'''
+'''set() interface: add, discard, clear, in, not in, |, &, ^,update(iteralbe)'''
+'''dict() interface: d[key]=value, key in d, key not in d, clear, update(iteralbe)'''
+
+
+
+
+
+#==========main===========
+
+
+
+
+
+
+
+
+
+
