158e8da192df3466f832e0a8ce08c2073ee6c552
zegabr <jgsp2@cin.ufpe.br>
2019-07-21 13:41:23 -0300

    fr rf mal acostumando

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -1,17 +1,19 @@
 #include <set>
 #include <map>
 #include <cmath>
+#include <deque>
 #include <queue>
 #include <stack>
 #include <bitset>
 #include <vector>
+#include <complex>
 #include <cstring>
 #include <iomanip>
 #include <sstream>
 #include <iostream>
 #include <algorithm>
-#include <unordered_set>
 #include <unordered_map>
+#include <unordered_set>
 
 #define endl '\n'
 #define ll long long
@@ -26,7 +28,7 @@ using namespace std;
 main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	cout<<fixed<<setprecision(20);	
-
+	//freopen("input","r",stdin);
 
 
 
--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -15,30 +15,24 @@
 #include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
-//#include <random>
-//#include <chrono>
+
 
 #define endl '\n'
 #define dif !=
 #define x first
 #define y second
 #define pb push_back
-#define mp make_pair
+#define fauto(k,c) for(auto &k : c)
 #define all(k) (k).begin(),(k).end()
 #define rall(k) (k).rbegin(),(k).rend()
 #define db(k) cerr << #k<< " = " << k << endl
-#define fr(i,a,b) for(int i=a; i<=b; i++) 
-#define rf(i,a,b) for(int i=a; i>=b; i--) 
-#define fauto(k,c) for(auto &k : c)
-#define PI acos(-1)
-
+using namespace std;
 #define pqueue priority_queue
 #define uset unordered_set
 #define umap unordered_map
 #define ld long double
 #define ll long long
 // #define int ll
-using namespace std;
 
 
 main(){
