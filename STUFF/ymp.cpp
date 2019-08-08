292aab9752429000a64bd72b8ab4e0f4a5cb1f8b
Jose Gabriel Silva Pereira <jgsp2@ug3c31.windows.cin.ufpe.br>
2019-08-08 09:18:41 -0300

    simpler, define other as needed

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -7,9 +7,12 @@
 #include <algorithm>
 using namespace std;
 
-#define sc1(a) scanf("%d", &(a))
-#define sc2(a,b) scanf("%d%d", &(a),&(b))
-#define sc3(a,b,c) scanf("%d%d%d", &(a),&(b),&(c))
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define ii pair<int,int>
+#define vii vector<ii>
+// #define int ll
 
 #define endl '\n'
 #define x first
@@ -17,16 +20,8 @@ using namespace std;
 #define pb push_back
 #define fauto(k,c) for(auto &k : c)
 #define all(k) (k).begin(),(k).end()
-#define rall(k) (k).rbegin(),(k).rend()
 #define db(k) cerr << #k<< " = " << k << endl
-#define popcount(a) __builtin_popcountll((long long)a)
 
-#define ll long long
-#define ld long double
-#define vi vector<int>
-#define ii pair<int,int>
-#define vii vector<ii>
-// #define int ll
 
 
 main(){
