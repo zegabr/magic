2eb1431ab87c0181f5cb194cd65727b8b7c54f43
zegabr <jgsp2@cin.ufpe.br>
2019-08-10 16:29:18 -0300

    fr and rf corrected

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #define vi vector<int>
 #define ii pair<int,int>
 #define vii vector<ii>
-// #define int ll
+#define int ll
 
 #define endl '\n'
 #define x first
@@ -20,14 +20,16 @@ using namespace std;
 #define pb push_back
 #define each(k,c) for(auto &k : c)
 #define all(k) (k).begin(),(k).end()
-#define fr(i,a,b) for(int i=a;i<=int(b);i++)
-#define rf(i,a,b) for(int i=a;i>=int(b);i--)
+#define fr(i,a,b) for(int i=a;i<=(int)b;i++)
+#define rf(i,a,b) for(int i=a;i>=(int)b;i--)
 #define db(k) cerr << #k<< " = " << k << endl
 
 
 
 main(){
  	ios::sync_with_stdio(0); cin.tie(0);
-	
+
+
+
 
 }
