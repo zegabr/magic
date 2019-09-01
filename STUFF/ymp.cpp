a84b1366f546dbd35b60279760cd202b762db12c
zegabr <jgsp2@cin.ufpe.br>
2019-09-01 14:38:55 -0300

    each is good

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -1,27 +1,38 @@
 #include <set>
 #include <map>
-#include <cmath>
 #include <vector>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cmath>
 #include <iostream>
 #include <algorithm>
 
 using namespace std;
-#define ll long long
 #define endl '\n'
 #define x first
 #define y second
 #define pb push_back
 #define all(k) (k).begin(),(k).end()
+#define each(a,c) for(auto &a:c)
 #define fr(i,a,b) for(int i=a;i<=(int)b;i++)
 #define rf(i,a,b) for(int i=a;i>=(int)b;i--)
 #define db(k) cerr << #k<< " = " << k << endl
+#define sc(d) scanf("%d",&d) 
+#define scll(d) scanf("%lld",&d) 
 
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define ii pair<int,int>
+#define vii vector<ii>
 
 
 
 int32_t main(){
  	ios::sync_with_stdio(0); cin.tie(0);
-
+	
 
 
 	return 0;
