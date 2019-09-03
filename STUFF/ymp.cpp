0531a8a47d33b41e697d542972cffb047d4c8eef
zegabr <jgsp2@cin.ufpe.br>
2019-09-03 19:43:31 -0300

    deque={queue/stack}

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -1,24 +1,25 @@
 #include <set>
 #include <map>
 #include <vector>
+#include <deque>
 #include <cmath>
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 #define endl '\n'
 #define x first
 #define y second
 #define pb push_back
-#define sc(d) scanf("%d",&d) 
-#define scll(d) scanf("%lld",&d) 
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define si(d) scanf("%d",&d) 
+#define sll(d) scanf("%lld",&d) 
 #define each(a,c) for(auto &a:c)
 #define all(k) (k).begin(),(k).end()
 #define fr(i,a,b) for(int i=a;i<=(int)b;i++)
 #define rf(i,a,b) for(int i=a;i>=(int)b;i--)
 #define db(k) cerr << #k<< " = " << k << endl
-
-
 #define ll long long
 #define ld long double
 #define vi vector<int>
