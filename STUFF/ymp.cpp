9a08168efb56b4a846e8de193e339618b80ab6e4
zegabr <jgsp2@cin.ufpe.br>
2019-09-04 18:57:09 -0300

    vii not used much

--- STUFF/ymp.cpp
+++ STUFF/ymp.cpp
@@ -17,17 +17,14 @@ using namespace std;
 #define ppf pop_front
 #define ll long long
 #define ld long double
-#define vi vector<int>
 #define ii pair<int,int>
-#define vii vector<ii>
 #define each(a,c) for(auto &a:c)
 #define all(k) (k).begin(),(k).end()
 #define fr(i,a,b) for(int i=a;i<=(int)b;i++)
 #define rf(i,a,b) for(int i=a;i>=(int)b;i--)
 #define db(k) cerr << #k<< " = " << k << endl
-
 #define getchar getchar_unlocked
-void si(int &num){//scan int
+void sc(int &num){//scan int
 	num=0;bool neg=0;
 	register int c = getchar();
 	if(c=='-'){
@@ -38,7 +35,7 @@ void si(int &num){//scan int
 		num=(num<<1)+(num<<3)+c-'0';
 	if(neg)num*=-1;
 }
-void si(ll &num){//scan ll
+void sc(ll &num){//scan ll
 	num=0ll;
 	bool neg=0;
 	register int c = getchar();
