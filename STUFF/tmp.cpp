6f64b9f01c9f227a3417c509cf7be89b723fd568
zegabr <jgsp2@cin.ufpe.br>
2019-09-21 18:26:34 -0300

    simpler tmp

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -1,15 +1,20 @@
-#include <bits/stdc++.h>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define x first
+#define y second
 #define ll long long
-#define endl '\n'
+#define ld long double
+#define ii pair<int,int>
 
 
-
-int32_t main(){
+int32_t main (){
 	ios::sync_with_stdio(0); cin.tie(0);
-
-
-
-
+	
+	
+	
+	
 	return 0;
 }
