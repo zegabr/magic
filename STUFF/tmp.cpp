0364f8d2b2bdf38c03a8ca7aec06daaf27291a72
zegabr <jgsp2@cin.ufpe.br>
2019-09-21 18:33:17 -0300

    paragraph for vim

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -10,11 +10,12 @@ using namespace std;
 #define ii pair<int,int>
 
 
-int32_t main (){
-	ios::sync_with_stdio(0); cin.tie(0);
+
+
+int32_t main(){
+ 	ios::sync_with_stdio(0); cin.tie(0);
 	
 	
-	
-	
+
 	return 0;
 }
