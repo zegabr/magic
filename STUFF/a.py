c27e38f22dace1b0e8807ae784d8fee0b4302a08
Jose Gabriel Silva Pereira <jgsp2@ug2c32.windows.cin.ufpe.br>
2019-09-23 15:22:38 -0300

    falta escrever no bashrc real

--- STUFF/a.py
+++ STUFF/a.py
@@ -1,8 +1,29 @@
 import os
 
 origin=open('./aliases','r')
-
 aliases=origin.read().split('\n')
+alia = []
+boo = False
+for ali in aliases:#pega aliases a ser colocado no bashrc
+	if ali == '##ALIASES':
+		boo = not boo
+	elif boo:
+		alia.append(ali)
 
-for ali in aliases:
+for ali in alia:
 	print(ali)
+
+from os.path import expanduser
+home = expanduser("~")
+bashrc = open(home+'/.bashrc','r')
+bash_text = bashrc.read().split('\n')
+bash_sem_alias = []
+boo = True
+for linha in bash_text:
+	if linha == '##ALIASES':
+		boo = not boo
+	elif boo:
+		bash_sem_alias.append(linha)
+
+for linha in bash_sem_alias:
+	print(linha)
