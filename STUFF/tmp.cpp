b5cadc57ba346d41976703220f876a8c656edf74
zegabr <jgsp2@cin.ufpe.br>
2019-12-13 01:06:51 -0300

    fr rf

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -19,6 +19,8 @@ using namespace std;
 #define all(k) k.begin(),k.end()
 #define rall(k) k.rbegin(),k.rend()
 #define fall(a,c) for(auto &a:c)
+#define fr(i,a,b) for(int i=a;i<=b;i++)
+#define rf(i,a,b) for(int i=a;i>=b;i--)
 #define ll long long
 #define ld long double
 #define ii pair<int,int>
