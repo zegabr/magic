0649e6aee410854953300065fae106b7cb4e4888
zegabr <jgsp2@cin.ufpe.br>
2019-12-15 22:20:55 -0300

    speed forces

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -17,6 +17,8 @@ using namespace std;
 #define fall(a,c) for(auto &a:c)
 #define all(k) k.begin(),k.end()
 #define rall(k) k.rbegin(),k.rend()
+#define fr(i,a,b) for(int i=a;i<=b;i++)
+#define rf(i,a,b) for(int i=a;i>=b;i--)
 #define ll long long
 #define ld long double
 #define ii pair<int,int>
