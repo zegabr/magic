e5846f8a038fe44cca341759798acd8565d6a382
Jose Gabriel Silva Pereira <jgsp2@ug3c21.windows.cin.ufpe.br>
2019-12-17 14:21:21 -0300

    go away

--- STUFF/tmp.cpp
+++ STUFF/tmp.cpp
@@ -19,8 +19,6 @@ using namespace std;
 #define ii pair<int,int>
 #define all(k) k.begin(),k.end()
 #define rall(k) k.rbegin(),k.rend()
-#define fr(i,a,b) for(int i=a;i<=b;i++)
-#define rf(i,a,b) for(int i=a;i>=b;i--)
 #define fa(a,c) for(auto &a:c)
 #define heap priority_queue
 
