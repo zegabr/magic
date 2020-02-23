d40689bd32d8bff97b0b0c4376338538f0c0fa04
zegabr <jgsp2@cin.ufpe.br>
2020-02-23 13:45:58 -0300

    stopped using gedit

--- n.sh
+++ n.sh
@@ -3,6 +3,6 @@
 
 cp ./m/clonegithub.sh ~/clonegithub.sh #this is important to run gitclone alias properly
 echo "==============x.cpp reseted================"
-source ./m/g.sh ##gedit setup
+#source ./m/g.sh ##gedit setup
 source ./m/v.sh ##vim setup
 source ./m/a.sh ##aliases
