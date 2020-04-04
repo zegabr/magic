ae0f415622364527a3595e945db57f3fef37c261
zegabr <jgsp2@cin.ufpe.br>
2020-04-03 23:15:27 -0300

    gedit is back?

--- n.sh
+++ n.sh
@@ -2,6 +2,6 @@
 #n not competitive
 
 cp ./m/clonegithub.sh ~/clonegithub.sh #this is important to run gitclone alias properly
-#source ./m/g.sh ##gedit setup
+source ./m/g.sh ##gedit setup
 source ./m/v.sh ##vim setup
 source ./m/a.sh ##aliases
