d8c6cd8bf0e37e93893bace8bf3211aa3a9f46d3
zegabr <jgsp2@cin.ufpe.br>
2020-05-27 14:29:25 -0300

    oh my zsh

--- t.sh
+++ t.sh
@@ -1,2 +1,12 @@
 #terminal setup here
+#part1
+sudo apt install zsh
+chsh -s $(which zsh)
 
+#oh my zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+#theme
+
+
+echo "unlog and relog on pc"
