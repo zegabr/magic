a8a8e7544d5fdf21c11f9c1f7f903e9c6afbd0a0
zegabr <jgsp2@cin.ufpe.br>
2020-06-29 12:00:14 -0300

    style(m.sh): add todo comment

--- n.sh
+++ n.sh
@@ -4,4 +4,4 @@
 cp ./m/clonegithub.sh ~/clonegithub.sh #this is important to run gitclone alias properly
 source ./m/g.sh ##gedit setup
 source ./m/v.sh ##vim setup
-source ./m/a.sh ##aliases
+source ./m/a.sh ##aliases # TODO: this is making bugs when oh my zsh is installed, remove this
