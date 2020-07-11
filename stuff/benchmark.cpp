64aa5be6c276633d3ec06c0c5d0d9e1379f2be78
Geovane Pereira <geeo.sp@gmail.com>
2020-07-11 12:14:31 -0300

    Added multithread benchmark

--- stuff/benchmark.cpp
+++ stuff/benchmark.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 #include<time.h>
-#include<math.h>
+#include<math.h>//use g++ -std=c++11 -pthread benchmark.cpp  to compile
+#include <thread>
+#include <mutex>   
 using namespace std;
+
+//may return 0 when not able to detect
+const auto processor_count = std::thread::hardware_concurrency();
+
+
+
+
+
 typedef long long ll;
 const int multiplier = 1000000;
 const int range = 1000;
@@ -13,7 +23,7 @@ int check(int m){
 	for(int i=0;i<lim;i++) c++;
 	t=clock()-t;
 	double ans = (double)t/CLOCKS_PER_SEC;
-	cout<<m<<"e6=>"<<ans<<"s\n";
+cout<<m<<"e6=>"<<ans<<"s\n";
 	if(fabs(ans-1.0)  <= 1e-2) return 0;
 	else if(ans < 1.0) return -1;
 	else return 1;
@@ -30,12 +40,46 @@ ll bs(int l, int r){
 	return r;
 }
 
+
+std::mutex mtx; 
+void  threaderedBenchmark(int l, int r, ll* answer){
+	//mulththread code
+	ll ra = bs( l,  r);
+	//~mulththread code
+	//inside the lock, there is only one thread running  this code
+	mtx.lock();
+	*answer+=ra;//add ra in the value stored in answer. ra is shared across threads
+	mtx.unlock();
+	//~single thread code
+
+}
+
+/*
+
+
 int main(){
 	
 	ll m = bs(1,range);
 	long double m2 = (long double)m/(range/10);
 	cout<<"this PC runs "<<m2<<"e8 operations in 1 sec\n";
 	
+	
+}*/
+int main(){
+	ll answer = 0;
+	
+	cout<<processor_count<<" threads available \n";
+	int l = 5;
+	//initialize the threads array
+	thread threads  [processor_count];
+	//start every thread
+	for(int i=0;i< processor_count;i++)
+		threads[i] = thread(threaderedBenchmark, l,range, &answer);
+	//wait for each thread to finish
+	for (auto& th : threads) th.join();
+
+	long double m2 = (long double)answer/(range/10);
+	cout<<"this PC runs "<<m2<<"e8 operations in "<<l<<" sec\n";
 	
 }
 
