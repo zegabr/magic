f2a6f613d8512e885f970e90d5b49c1b06fc63e8
zegabr <jgsp2@cin.ufpe.br>
2020-07-11 18:32:30 -0300

    benchmark.cpp faster using constant time opetarions

--- stuff/benchmark.cpp
+++ stuff/benchmark.cpp
@@ -2,40 +2,24 @@
 #include<time.h>
 #include<math.h>
 using namespace std;
-typedef long long ll;
-const int multiplier = 1000000;
-const int range = 1000;
 
-int check(int m){
-    int c=0;
-    ll lim=m*multiplier;
-    clock_t t=clock();
-    for(int i=0;i<lim;i++) c++;
-    t=clock()-t;
-    double ans = (double)t/CLOCKS_PER_SEC;
-    cout<<m<<"e6=>"<<ans<<"s\n";
-    if(fabs(ans-1.0)  <= 1e-2) return 0;
-    else if(ans < 1.0) return -1;
-    else return 1;
-}
+#define FIXED_ITERATIONS_QUANTITY 1e9 //good enough to be near 10 sec
 
-ll bs(int l, int r){
-    while(l<=r){
-        ll m=(l+r)/2;
-        int k = check(m);
-        if(k==0) return m;
-        if(k==-1) l=m+1;
-        else r=m-1;
+double getSecondsToIterate(int iterations = FIXED_ITERATIONS_QUANTITY){
+    int c = 0;
+    clock_t t = clock();
+    for(int i = 0; i < iterations; i++){
+        c++;
     }
-    return r;
+    t = clock() - t;
+    return (double)t/CLOCKS_PER_SEC;
 }
+
 
 int main(){
-
-    ll m = bs(1,range);
-    long double m2 = (long double)m/(range/10);
-    cout<<"this PC runs "<<m2<<"e8 operations in 1 sec\n";
-
-
+    int iterations = FIXED_ITERATIONS_QUANTITY;
+    double sec = getSecondsToIterate(iterations);
+    cout<<"this PC runs "<< (double)iterations << " operations in " << sec <<" seconds" << endl ;
+    cout<< "then, it runs "<< (double)iterations/sec << " operations in 1 sec." << endl ;
 }
 
