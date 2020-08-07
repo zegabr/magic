987feaad10e2e1374c3d8e913c7c85231253317a
zegabr <jgsp2@cin.ufpe.br>
2020-08-06 21:32:19 -0300

    fix

--- stuff/extra-bashrc.sh
+++ stuff/extra-bashrc.sh
@@ -27,6 +27,21 @@ function _git_log_prettily(){
   fi
 }
 
+function git_current_branch() {
+  local ref
+  ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2> /dev/null)
+  local ret=$?
+  if [[ $ret != 0 ]]; then
+    [[ $ret == 128 ]] && return  # no git repo.
+    ref=$(__git_prompt_git rev-parse --short HEAD 2> /dev/null) || return
+  fi
+  echo ${ref#refs/heads/}
+}
+
+function current_branch() {
+  git_current_branch
+}
+
 # Check if main exists and use instead of master
 function git_main_branch() {
   if [[ -n "$(git branch --list main)" ]]; then
