8c40d47e68e09f2dabc9b03feaf7513f263d1f5d
zegabr <jgsp2@cin.ufpe.br>
2020-11-06 20:48:28 -0300

    remove pyutils comment tag

--- z.sh
+++ z.sh
@@ -7,10 +7,10 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 git clone https://github.com/zegabr/pyutils
 
 # Change theme below
-python3 pyutils/replace_line_with_content.py ~/.zshrc "ZSH_THEME" "ZSH_THEME=\"af-magic\"" "#"
-python3 pyutils/replace_line_with_content.py ~/.zshrc "CASE_SENSITIVE" "CASE_SENSITIVE=\"true\"" "#"
-python3 pyutils/replace_line_with_content.py ~/.zshrc "DISABLE_UPDATE_PROMPT" "DISABLE_UPDATE_PROMPT=\"true\"" "#"
-python3 pyutils/replace_line_with_content.py ~/.zshrc "source /etc/zsh_command_not_found" "source /etc/zsh_command_not_found" "#"
+python3 pyutils/replace_line_with_content.py ~/.zshrc "ZSH_THEME" "ZSH_THEME=\"af-magic\""
+python3 pyutils/replace_line_with_content.py ~/.zshrc "CASE_SENSITIVE" "CASE_SENSITIVE=\"true\""
+python3 pyutils/replace_line_with_content.py ~/.zshrc "DISABLE_UPDATE_PROMPT" "DISABLE_UPDATE_PROMPT=\"true\""
+python3 pyutils/replace_line_with_content.py ~/.zshrc "source /etc/zsh_command_not_found" "source /etc/zsh_command_not_found"
 
 # Cloning extra plugins
 cd ~/.oh-my-zsh/custom/plugins
@@ -19,12 +19,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions
 cd -
 
 # Add plugins below
-python3 pyutils/replace_line_with_content.py ~/.zshrc "plugins=(" "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)" "#"
+python3 pyutils/replace_line_with_content.py ~/.zshrc "plugins=(" "plugins=(git zsh-syntax-highlighting zsh-autosuggestions)"
 
 
 source ~/.zshrc
-
-source ./a.sh # add aliases
 
 echo "removing pyutils"
 rm -rf pyutils/
