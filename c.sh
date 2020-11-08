8d2c6954792608c289b40523a7ce709fd9b493e3
zegabr <jgsp2@cin.ufpe.br>
2020-11-08 20:48:34 -0300

    moved cp aliases to cp repo

--- c.sh
+++ c.sh
@@ -1,24 +1,16 @@
 # competitive setup
 
-#initial setup
-source ./a.sh # aliases
-
 #creates competitive/ at parent of m/
 cd ../
 rm -rf competitive/
 mkdir competitive/
-
 cd competitive/
 
 #copy my competitive code lib
-rm -rf cp/
 git clone git@github.com:zegabr/cp.git
 echo 
+cd cp
 
 pwd
-source cp/init.sh
-
-echo "cp folder has:"
-ls cp/
-
-pwd
+source init.sh
+pwd #init changes directory
--- stuff/aliases.sh
+++ stuff/aliases.sh
@@ -1,18 +1,11 @@
 ##ALIASES
 
-#chore
 alias atualiza='sudo apt update && sudo apt -u upgrade -y && sudo apt autoclean -y && sudo apt clean -y && sudo apt autoremove -y'
 alias lag='alias | grep'
 alias gclone='bash ~/clonegithub.sh'
 alias gs='git status'
 
-#competitive
-alias m='make && make t'
-alias c='g++ -std=c++17 -O2 -Wshadow -Wall -Wno-unused-result'
-alias tc='time g++ -std=c++17 -O2'
-alias cw='g++ -std=c++17 -O2 -Wfatal-errors'
 
-#remaper
 alias remap-keys='bash ~/remaper/remaper.sh'
 alias undo-remap-keys='bash ~/remaper/undo.sh'
 
