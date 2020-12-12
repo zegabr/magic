84275d4c93e4fbfc0e17e8c575432b2b03049894
zegabr <jgsp2@cin.ufpe.br>
2020-12-12 12:03:28 -0300

    add custom keybinds script and refactor

--- m.sh
+++ m.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 # m for main
 
-cd m/
-
-bash ./ssh.sh
 source ./z.sh # z-shell (also install oh-my-zsh update its theme and plugins)
-source ./a.sh # aliases 
+source ./a.sh # aliases
+source ./stuff/custom-keybindings.sh
 
