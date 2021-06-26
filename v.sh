486b179dd30b1e3d37faa6cc6a0e8275cc578e4f
Jose <jgsp2@cin.ufpe.br>
2021-06-26 19:29:38 -0300

    add ripgrep to v.sh

--- v.sh
+++ v.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # v for vim
+
+# install ripgrep
+# curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb
+# sudo dpkg -i ripgrep_12.1.1_amd64.deb
+# rm ripgrep_12.1.1_amd64.deb
+
 cp ./stuff/.vimrc ~/.vimrc
 echo
 echo "========my vim setup overrided yours======="
